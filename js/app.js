718948f823727c19897e17a073177b078cbadf63
Draquix <Draquix@yahoo.com>
2021-01-06 16:23:58 -0600

    almost complete with the javascript- this is the test run that might be finished product

--- js/app.js
+++ js/app.js
@@ -44,4 +44,48 @@ for (var i = 0; i < questions.length; i++) {
         }
     }
 }
-alert("You got " + score + " out of 5 questions correct.");
+//random number generation from: w3schools.com/js/js_random.asp
+var secretGuess = Math.floor(Math.random() * 10) + 1;
+for (var i = 0; i < 4; i++) {
+    var input = prompt("Guess a number between 1 and 10");
+    console.log(i + 1 + " is the iteration.");
+    //for strict comparison the string from the user input must be converted to a number.
+    //outside instruction gained from: https://gomakethings.com/converting-strings-to-numbers-with-vanilla-javascript/
+    var inputInt = parseInt(input, 10);
+    console.log("iteration number " + (i+1));
+    if (inputInt === secretGuess) {
+        alert("Hooray! You guessed the secret number!");
+        console.log("Secret number has been successfully guessed.");
+        score ++;
+        break;
+    } else if (inputInt < secretGuess) {
+        alert("The number you guessed is lower than the secret number.");
+        console.log("The guess was lower than the secret number");
+    } else if (inputInt > secretGuess) {
+        alert("The number you guessed is higher than the secret number.");
+        //console.log("The guess was higher than the secret number");
+    } else {
+        alert("There is an error, this message shouldn't be reached.");
+        console.log("Error.");
+    }
+}
+alert("The number you were trying to guess was: " + secretGuess);
+
+var favoriteMovies = ["Forest Gump","Star Wars","Being John Malkovich","The Fifth Element","Lord of the Rings"];
+for (var i = 0; i < 6; i++) {
+    var guess = prompt("What do you think one of my favorite movies are? There are multiple correct answers.");
+    for (var x = 0; x < favoriteMovies.length; x++) {
+        if (guess.toUpperCase() === favoriteMovies[x].toUpperCase()) {
+            alert("You are correct, I love "+ guess + "!");
+            score ++;
+            var allDone = true;
+            break;
+        }
+    }
+    if (allDone === true){
+        break;
+    }
+    alert("Nope, try again...");
+}
+
+alert("You got " + score + " out of 7 questions correct.");
